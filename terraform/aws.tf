651af39d8ffc1cbe4f563c1d6b2a9e54b1f656d3
Fernando Partida <ferpart@hotmail.com>
2021-01-17 23:27:33 -0600

    terraform folder re-structure

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -141,20 +141,20 @@ resource "aws_instance" "web-server-instance" {
   user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update -y
-              sudo apt-get install \
+              sudo apt-get -y install \
                 apt-transport-https \
                 ca-certificates \
                 curl \
                 gnupg-agent \
-                software-properties-common -y
+                software-properties-common
                 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
                 sudo apt-key fingerprint 0EBFCD88
                  sudo add-apt-repository \
                     "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
                     $(lsb_release -cs) \
                     stable"
-                sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-                sudo apt-get install docker-compose -y
+                sudo apt-get -y install docker-ce docker-ce-cli containerd.io
+                sudo apt-get -y install docker-compose
               EOF
 
   tags = {
